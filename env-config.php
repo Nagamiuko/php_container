ac55bf587bc880a5c10fe491860b5960f0ea29a8
put kongka <nagamiuko@gmail.com>
2026-01-12 15:28:18 +0700

    aaa

---
+++ env-config.php
@@ -0,0 +1,10 @@
+<?php
+// config database 
+ define("DATABASE_HOSTNAME","localhost");  // localhost ที่ใช่ปัจจุบัน
+ define("DATABASE_USERNAME","root"); // ชื่อผู้ใช้ database ปัจจุบัน 
+ define("DATABASE_PASSWORD",""); // รหัสผ่าน database ปัจจุบัน 
+ define("DATABASE_NAME","tavel"); // ชื่อ database ปัจจุบัน 
+// config url api 
+ define("URL_API","http://98.70.109.196:9000/api/"); // localhost:post  ที่ใช่ปัจจุบัน; // localhost:post  ที่ใช่ปัจจุบัน
+ define("URL_HOST","http://98.70.109.196:9000/");
+?>
